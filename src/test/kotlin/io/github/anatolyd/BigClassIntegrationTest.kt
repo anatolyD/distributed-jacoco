ac446796368bad6468e56fcbd061b2e3b5b40269
Anatoly Danilov <anatoly.danilov@anonossoftware.com>
2021-01-30 22:40:41 +0300

    demo slow execution

--- src/test/kotlin/io/github/anatolyd/BigClassIntegrationTest.kt
+++ src/test/kotlin/io/github/anatolyd/BigClassIntegrationTest.kt
@@ -3,10 +3,12 @@ package io.github.anatolyd
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
+import java.util.concurrent.TimeUnit
 
 internal class BigClassIntegrationTest {
     @Test
     internal fun `integration test`() {
+        Thread.sleep(TimeUnit.MINUTES.toMillis(7))
         assertEquals("big", BigClass().haveFun())
     }
 }
--- src/test/kotlin/io/github/anatolyd/SmallClassTest.kt
+++ src/test/kotlin/io/github/anatolyd/SmallClassTest.kt
@@ -2,10 +2,12 @@ package io.github.anatolyd
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.util.concurrent.TimeUnit
 
 internal class SmallClassTest {
     @Test
-    internal fun `small test`() {
+    internal fun `small tests`() {
+        Thread.sleep(TimeUnit.MINUTES.toMillis(5))
         assertEquals("small", SmallClass().haveFun())
     }
 }
